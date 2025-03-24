cd7d22c386535af0c1b017143522a36418a26308
Roger <github@betechie.com>
2025-03-24 13:32:30 -0700

    2.1 Autonomous Core Loop Module Initialization

---
+++ packages/plugin-autonomous/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Autonomous Plugin for Eliza OS
+ * 
+ * This plugin provides autonomous operation capabilities for Eliza agents,
+ * including continuous event loops, planning, and more.
+ */
+
+export { AutonomousLoop, type TriggerCallback } from './AutonomousLoop'; 
